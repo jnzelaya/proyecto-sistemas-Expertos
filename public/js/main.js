67d37389052c24cea791c3a2b64665bc37b166dd
jnzelaya <jnzelaya91@gmail.com>
2018-07-14 16:19:14 -0600

    modificcion de registro

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,9 @@ window.addEventListener("load", function() {
 	boton.addEventListener("click",function(){
 		var barra=document.querySelector("nav");
 		var bg=barra.style.background
-		if(bg=="gray"){
-			barra.style.background="green";
+		if(bg==""){
+			barra.style.background="gray";
 		}else{
-		color=barra.style.background="gray";}
+		     barra.style.background="";}
 	});
 });
